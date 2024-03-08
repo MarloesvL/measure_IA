2c6235398ef285d30dc021bf82ccf4405b8a4aa7
MarloesvL <mvanloon98@gmail.com>
2024-03-08 10:32:24 +0100

    fix: wrong mass name in measure_rotational_velocity_single

--- src/measure_IA_multiprocessing.py
+++ src/measure_IA_multiprocessing.py
@@ -891,7 +891,7 @@ class MeasureVariablesSnapshotMultiprocessing(SimInfo):
 		for n in indices:
 			off_n = self.off[n]
 			len_n = self.Len[n]
-			mass = self.mass_subhalo[n]
+			mass = self.mass[n]
 			if len_n < 2:
 				avg_rot_vel.append(0)
 				vel_disp.append(0)
