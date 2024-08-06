ce8a77b8b3e4966e40d529aa7d69e247e691dd57
MarloesvL <mvanloon98@gmail.com>
2024-08-06 10:35:40 +0200

    feat: create cov combining mock data

--- create_mock_data.py
+++ create_mock_data.py
@@ -51,3 +51,103 @@ mock_Snap.close()
 #
 #
 # mock_Shapes.close()
+
+
+# mock IA data
+path_proc = './tests/data/processed/'
+mock_IA = h5py.File(f"{path_proc}/{sim}/IA_correlations.hdf5", "a")
+group_wgplus = create_group_hdf5(mock_IA, f"Snapshot_99/w_g_plus")
+group_multi = create_group_hdf5(mock_IA, f"Snapshot_99/multipoles_g_plus")
+data1 = np.array([0, 1, 0, 3])
+data2 = np.array([1, 3, 2, 1])
+data3 = np.array([2, 2, 1, 2])
+# means 1,2,1,2
+# dev1 -1,-1,-1,1
+# dev2 0,1,1,-1
+# dev3 1,0,0,0
+write_dataset_hdf5(group_wgplus, "set1_0", data1)
+write_dataset_hdf5(group_wgplus, "set1_1", data2)
+write_dataset_hdf5(group_wgplus, "set1_2", data3)
+# cov (*2/3)
+# 2		1	1	-1
+# 1		2	2	-2
+# 1		2	2	-2
+# -1   -2  -2	 2
+
+data1 = np.array([2, 2, 3, 0])
+data2 = np.array([1, 0, 1, 2])
+data3 = np.array([3, 1, 2, 1])
+# means 2,1,2,1
+# dev1 0, 1, 1, -1
+# dev2 -1,-1,-1,1
+# dev3 1,0,0,0
+write_dataset_hdf5(group_wgplus, "set2_0", data1)
+write_dataset_hdf5(group_wgplus, "set2_1", data2)
+write_dataset_hdf5(group_wgplus, "set2_2", data3)
+# cov (*2/3)
+#  2	1	1	-1
+#  1	2	2	-2
+#  1	2	2	-2
+# -1	-2	-2	2
+
+# comb1&2
+# dev1 -1,-1,-1, 1
+# dev1 0, 1,  1, -1
+
+# dev2 0, 1,  1,-1
+# dev2 -1,-1,-1, 1
+
+# dev3 1,0,0,0
+# dev3 1,0,0,0
+
+# comb cov (*2/3)
+# 1	-1	-1	1
+# -1-2	-2	2
+# -1 -2	-2	2
+# 1	2	2	-2
+
+
+data1 = 2 * np.array([2, 2, 3, 0])
+data2 = 2 * np.array([1, 0, 1, 2])
+data3 = 2 * np.array([3, 1, 2, 1])
+# means 4,2,4,2
+# dev1 0, 2, 2, -2
+# dev2 -2,-2,-2,2
+# dev3 2,0,0,0
+write_dataset_hdf5(group_wgplus, "set3_0", data1)
+write_dataset_hdf5(group_wgplus, "set3_1", data2)
+write_dataset_hdf5(group_wgplus, "set3_2", data3)
+
+
+# comb 1&3
+# dev1 -1,-1,-1,1
+# dev1 0, 2, 2, -2
+
+# dev2 0,1,  1,-1
+# dev2 -2,-2,-2,2
+
+# dev3 1,0,0,0
+# dev3 2,0,0,0
+
+# 2	-2	-2	2
+# -2 -4	-4	4
+# -2 -4	-4	4
+# 2	4	4	-4
+
+# comb 2&3
+
+# dev2 0,1,  1,-1
+# dev1 0, 2, 2, -2
+
+# dev1 -1,-1,-1,1
+# dev2 -2,-2,-2,2
+
+# dev3 1,0,0,0
+# dev3 2,0,0,0
+
+#  4	2	2	-2
+#  2	4	4	-4
+#  2	4	4	-4
+# -2	-4	-4	4
+
+mock_IA.close()
