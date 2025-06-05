c5c35a21e77b5de8f1297f8ba3e8a79ec1b82617
MarloesvL <mvanloon98@gmail.com>
2025-06-05 12:39:46 +0200

    feat: tests for internal consistency sims: comparing to saved output, comparing gg and g+ to both and multiprocessing & tree methods

--- tests/test_m_sim_internal_consistency.py
+++ tests/test_m_sim_internal_consistency.py
@@ -10,8 +10,8 @@ def test_compare_saved_output(IA_mock_TNG300_n8):
 	:return:
 	'''
 	IA_mock_TNG300_n8.measure_xi_multipoles("All_both", 'both', 8, file_tree_path='./data/processed/')
-	output_wgp = ReadData("TNG300", "test_IA_mock_TNG300", 99, "multipoles_g_plus/", "All_both", './data/processed/')
-	output_wgg = ReadData("TNG300", "test_IA_mock_TNG300", 99, "multipoles_gg/", "All_both", './data/processed/')
+	output_wgp = ReadData("TNG300", "test_IA_mock_TNG300", 99, "multipoles_g_plus/", data_path='./data/processed/')
+	output_wgg = ReadData("TNG300", "test_IA_mock_TNG300", 99, "multipoles_gg/", data_path='./data/processed/')
 	wgp = output_wgp.read_cat("All_both")
 	rp_wgp = output_wgp.read_cat("All_both_r")
 	cov_wgp = output_wgp.read_cat("All_both_jackknife_cov_8")
@@ -19,8 +19,8 @@ def test_compare_saved_output(IA_mock_TNG300_n8):
 	rp_wgg = output_wgg.read_cat("All_both_r")
 	cov_wgg = output_wgg.read_cat("All_both_jackknife_cov_8")
 
-	available_out_wgp = ReadData("TNG300", "mock_IA_TNG300", 99, "multipoles_g_plus/", "All_both", './data/processed/')
-	available_out_wgg = ReadData("TNG300", "mock_IA_TNG300", 99, "multipoles_gg/", "All_both", './data/processed/')
+	available_out_wgp = ReadData("TNG300", "mock_IA_TNG300", 99, "multipoles_g_plus/", data_path='./data/processed/')
+	available_out_wgg = ReadData("TNG300", "mock_IA_TNG300", 99, "multipoles_gg/", data_path='./data/processed/')
 	a_wgp = available_out_wgp.read_cat("All")
 	a_rp_wgp = available_out_wgp.read_cat("All_r")
 	a_cov_wgp = available_out_wgp.read_cat("All_jackknife_cov_8")
@@ -44,8 +44,8 @@ def test_gg_gp_both(IA_mock_TNG300_n8):
 	IA_mock_TNG300_n8.measure_xi_multipoles("All_gp", 'g+', 8, file_tree_path='./data/processed/')
 	IA_mock_TNG300_n8.measure_xi_multipoles("All_gg", 'gg', 8, file_tree_path='./data/processed/')
 
-	output_wgp = ReadData("TNG300", "test_IA_mock_TNG300", 99, "multipoles_g_plus/", "All_both", './data/processed/')
-	output_wgg = ReadData("TNG300", "test_IA_mock_TNG300", 99, "multipoles_gg/", "All_both", './data/processed/')
+	output_wgp = ReadData("TNG300", "test_IA_mock_TNG300", 99, "multipoles_g_plus/", data_path='./data/processed/')
+	output_wgg = ReadData("TNG300", "test_IA_mock_TNG300", 99, "multipoles_gg/", data_path='./data/processed/')
 	wgp_b = output_wgp.read_cat("All_both")
 	rp_wgp_b = output_wgp.read_cat("All_both_r")
 	wgg_b = output_wgg.read_cat("All_both")
@@ -70,6 +70,35 @@ def test_gg_gp_both(IA_mock_TNG300_n8):
 	return
 
 
+def test_multiprocessing_versions(IA_mock_TNG300_n1_large, IA_mock_TNG300_n8_large, IA_mock_TNG300_n17_large):
+	IA_mock_TNG300_n1_large.measure_xi_multipoles("n1", 'both', 8, file_tree_path='./data/processed/')
+	IA_mock_TNG300_n8_large.measure_xi_multipoles("n8", 'both', 8, file_tree_path='./data/processed/')
+	IA_mock_TNG300_n17_large.measure_xi_multipoles("n17", 'both', 8, file_tree_path='./data/processed/')
+
+	output_wgp = ReadData("TNG300", "test_IA_mock_TNG300_large", 99, "multipoles_g_plus/", data_path=
+	'./data/processed/')
+	wgp_n1 = output_wgp.read_cat("n1")
+	wgp_n8 = output_wgp.read_cat("n8")
+	wgp_n17 = output_wgp.read_cat("n17")
+
+	rp_n1 = output_wgp.read_cat("n1_r")
+	rp_n8 = output_wgp.read_cat("n8_r")
+	rp_n17 = output_wgp.read_cat("n17_r")
+
+	cov_n1 = output_wgp.read_cat("n1_jackknife_cov_8")
+	cov_n8 = output_wgp.read_cat("n8_jackknife_cov_8")
+	cov_n17 = output_wgp.read_cat("n17_jackknife_cov_8")
+
+	np.testing.assert_array_equal(rp_n1, rp_n8)
+	np.testing.assert_array_equal(rp_n1, rp_n17)
+
+	np.testing.assert_array_equal(wgp_n1, wgp_n8)
+	np.testing.assert_allclose(wgp_n1, wgp_n17, 1e-1)  # numerical differences
+
+	np.testing.assert_array_equal(cov_n1, cov_n8)
+
+
 if __name__ == '__main__':
 	test_compare_saved_output()
 	test_gg_gp_both()
+	test_multiprocessing_versions()
--- tests/test_w_sim_internal_consistency.py
+++ tests/test_w_sim_internal_consistency.py
@@ -10,8 +10,8 @@ def test_compare_saved_output(IA_mock_TNG300_n8):
 	:return:
 	'''
 	IA_mock_TNG300_n8.measure_xi_w("All_both", 'both', 8, file_tree_path='./data/processed/')
-	output_wgp = ReadData("TNG300", "test_IA_mock_TNG300", 99, "w_g_plus/", "All_both", './data/processed/')
-	output_wgg = ReadData("TNG300", "test_IA_mock_TNG300", 99, "w_gg/", "All_both", './data/processed/')
+	output_wgp = ReadData("TNG300", "test_IA_mock_TNG300", 99, "w_g_plus/", data_path='./data/processed/')
+	output_wgg = ReadData("TNG300", "test_IA_mock_TNG300", 99, "w_gg/", data_path='./data/processed/')
 	wgp = output_wgp.read_cat("All_both")
 	rp_wgp = output_wgp.read_cat("All_both_rp")
 	cov_wgp = output_wgp.read_cat("All_both_jackknife_cov_8")
@@ -19,8 +19,8 @@ def test_compare_saved_output(IA_mock_TNG300_n8):
 	rp_wgg = output_wgg.read_cat("All_both_rp")
 	cov_wgg = output_wgg.read_cat("All_both_jackknife_cov_8")
 
-	available_out_wgp = ReadData("TNG300", "mock_IA_TNG300", 99, "w_g_plus/", "All_both", './data/processed/')
-	available_out_wgg = ReadData("TNG300", "mock_IA_TNG300", 99, "w_gg/", "All_both", './data/processed/')
+	available_out_wgp = ReadData("TNG300", "mock_IA_TNG300", 99, "w_g_plus/", data_path='./data/processed/')
+	available_out_wgg = ReadData("TNG300", "mock_IA_TNG300", 99, "w_gg/", data_path='./data/processed/')
 	a_wgp = available_out_wgp.read_cat("All")
 	a_rp_wgp = available_out_wgp.read_cat("All_rp")
 	a_cov_wgp = available_out_wgp.read_cat("All_jackknife_cov_8")
@@ -44,8 +44,8 @@ def test_gg_gp_both(IA_mock_TNG300_n8):
 	IA_mock_TNG300_n8.measure_xi_w("All_gp", 'g+', 8, file_tree_path='./data/processed/')
 	IA_mock_TNG300_n8.measure_xi_w("All_gg", 'gg', 8, file_tree_path='./data/processed/')
 
-	output_wgp = ReadData("TNG300", "test_IA_mock_TNG300", 99, "w_g_plus/", "All_both", './data/processed/')
-	output_wgg = ReadData("TNG300", "test_IA_mock_TNG300", 99, "w_gg/", "All_both", './data/processed/')
+	output_wgp = ReadData("TNG300", "test_IA_mock_TNG300", 99, "w_g_plus/", data_path='./data/processed/')
+	output_wgg = ReadData("TNG300", "test_IA_mock_TNG300", 99, "w_gg/", data_path='./data/processed/')
 	wgp_b = output_wgp.read_cat("All_both")
 	rp_wgp_b = output_wgp.read_cat("All_both_rp")
 	wgg_b = output_wgg.read_cat("All_both")
@@ -60,9 +60,6 @@ def test_gg_gp_both(IA_mock_TNG300_n8):
 	cov_wgp = output_wgp.read_cat("All_gp_jackknife_cov_8")
 	cov_wgg = output_wgg.read_cat("All_gg_jackknife_cov_8")
 
-	print(cov_wgg)
-	print(cov_wgg_b)
-
 	np.testing.assert_array_equal(wgp, wgp_b)
 	np.testing.assert_array_equal(rp_wgp, rp_wgp_b)
 	np.testing.assert_array_equal(wgg, wgg_b)
@@ -73,6 +70,36 @@ def test_gg_gp_both(IA_mock_TNG300_n8):
 	return
 
 
+def test_multiprocessing_versions(IA_mock_TNG300_n1_large, IA_mock_TNG300_n8_large, IA_mock_TNG300_n17_large):
+	IA_mock_TNG300_n1_large.measure_xi_w("n1", 'both', 8, file_tree_path='./data/processed/')
+	IA_mock_TNG300_n8_large.measure_xi_w("n8", 'both', 8, file_tree_path='./data/processed/')
+	IA_mock_TNG300_n17_large.measure_xi_w("n17", 'both', 8, file_tree_path='./data/processed/')
+
+	output_wgp = ReadData("TNG300", "test_IA_mock_TNG300_large", 99, "w_g_plus/", data_path='./data/processed/')
+	wgp_n1 = output_wgp.read_cat("n1")
+	wgp_n8 = output_wgp.read_cat("n8")
+	wgp_n17 = output_wgp.read_cat("n17")
+
+	rp_n1 = output_wgp.read_cat("n1_rp")
+	rp_n8 = output_wgp.read_cat("n8_rp")
+	rp_n17 = output_wgp.read_cat("n17_rp")
+
+	cov_n1 = output_wgp.read_cat("n1_jackknife_cov_8")
+	cov_n8 = output_wgp.read_cat("n8_jackknife_cov_8")
+	cov_n17 = output_wgp.read_cat("n17_jackknife_cov_8")
+
+	np.testing.assert_array_equal(rp_n1, rp_n8)
+	np.testing.assert_array_equal(rp_n1, rp_n17)
+
+	np.testing.assert_array_equal(wgp_n1, wgp_n8)
+	np.testing.assert_allclose(wgp_n1, wgp_n17, 1e-1)  # numerical differences
+
+	np.testing.assert_array_equal(cov_n1, cov_n8)
+
+
+
+
 if __name__ == '__main__':
 	test_compare_saved_output()
 	test_gg_gp_both()
+	test_multiprocessing_versions()
