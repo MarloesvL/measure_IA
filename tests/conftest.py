c8a5a10947600d397e515d382f967e1771f7244f
MarloesvL <mvanloon98@gmail.com>
2025-10-23 14:16:15 +0200

    fix: update tests to work for new MeasureIABox version

--- tests/conftest.py
+++ tests/conftest.py
@@ -14,6 +14,22 @@ from measureia import MeasureIABox
 # def demo_sim(a):
 # 	return a**2
 
+@pytest.fixture()
+def IA_mock_TNG300_n1():
+	data_file = h5py.File('./data/raw/TNG300/mock_TNG300.hdf5', 'a')
+	COM = data_file["COM"][:]
+	Semimajor_Axis_Direction = data_file["Semimajor_Axis_Direction"][:]
+	q = data_file["q"][:]
+	data_file.close()
+	data_dir = {
+		"Position": COM,
+		"Position_shape_sample": COM,
+		"Axis_Direction": Semimajor_Axis_Direction,
+		"LOS": 2,
+		"q": q
+	}
+	return MeasureIABox(data_dir, f"./data/processed/TNG300/test_IA_mock_TNG300.hdf5", "TNG300",
+						99, [0.1, 20], 10, 8, None, num_nodes=1)
 
 @pytest.fixture()
 def IA_mock_TNG300_n8():
@@ -34,63 +50,6 @@ def IA_mock_TNG300_n8():
 
 
 @pytest.fixture()
-def IA_mock_TNG300_n1_large():
-	data_file = h5py.File('./data/raw/TNG300/mock_TNG300_large.hdf5', 'a')
-	COM = data_file["COM"][:]
-	Semimajor_Axis_Direction = data_file["Semimajor_Axis_Direction"][:]
-	q = data_file["q"][:]
-	data_file.close()
-	data_dir = {
-		"Position": COM,
-		"Position_shape_sample": COM,
-		"Axis_Direction": Semimajor_Axis_Direction,
-		"LOS": 2,
-		"q": q
-	}
-	return MeasureIABox(data_dir, f"./data/processed/TNG300/test_IA_mock_TNG300_large.hdf5",
-					 "TNG300", 99, [0.1, 20], 10,
-						8, None, num_nodes=1)
-
-
-@pytest.fixture()
-def IA_mock_TNG300_n8_large():
-	data_file = h5py.File('./data/raw/TNG300/mock_TNG300_large.hdf5', 'a')
-	COM = data_file["COM"][:]
-	Semimajor_Axis_Direction = data_file["Semimajor_Axis_Direction"][:]
-	q = data_file["q"][:]
-	data_file.close()
-	data_dir = {
-		"Position": COM,
-		"Position_shape_sample": COM,
-		"Axis_Direction": Semimajor_Axis_Direction,
-		"LOS": 2,
-		"q": q
-	}
-	return MeasureIABox(data_dir, f"./data/processed/TNG300/test_IA_mock_TNG300_large.hdf5",
-					 "TNG300", 99, [0.1, 20], 10, 8,
-						None, num_nodes=8)
-
-
-@pytest.fixture()
-def IA_mock_TNG300_n17_large():
-	data_file = h5py.File('./data/raw/TNG300/mock_TNG300_large.hdf5', 'a')
-	COM = data_file["COM"][:]
-	Semimajor_Axis_Direction = data_file["Semimajor_Axis_Direction"][:]
-	q = data_file["q"][:]
-	data_file.close()
-	data_dir = {
-		"Position": COM,
-		"Position_shape_sample": COM,
-		"Axis_Direction": Semimajor_Axis_Direction,
-		"LOS": 2,
-		"q": q
-	}
-	return MeasureIABox(data_dir, f"./data/processed/TNG300/test_IA_mock_TNG300_large.hdf5",
-					 "TNG300", 99, [0.1, 20], 10,
-						8, None, num_nodes=17)
-
-
-@pytest.fixture()
 def IA_available_output():
 	return MeasureIABox(None, f"./data/processed/TNG300/mock_IA_TNG300.hdf5",
 						"TNG300", 99, [0.1, 20], 10,
@@ -108,41 +67,3 @@ def jk_regions():
 		"q": np.array([]),
 	}
 	return MeasureIABox(data_dict, f"./data/processed/TNG300/no_write.hdf5", None, None, boxsize=3.)
-
-
-@pytest.fixture()
-def IA_mock_TNG300_jk():
-	data_file = h5py.File('./data/raw/TNG300/mock_TNG300.hdf5', 'a')
-	COM = data_file["COM"][:]
-	Semimajor_Axis_Direction = data_file["Semimajor_Axis_Direction"][:]
-	q = data_file["q"][:]
-	data_file.close()
-	data_dir = {
-		"Position": COM,
-		"Position_shape_sample": COM,
-		"Axis_Direction": Semimajor_Axis_Direction,
-		"LOS": 2,
-		"q": q
-	}
-	return MeasureIABox(data_dir, f"./data/processed/TNG300/test_IA_mock_TNG300_jk.hdf5",
-						"TNG300", 99, [0.1, 20], 10,
-						8, None)
-
-
-@pytest.fixture()
-def IA_mock_TNG300_jk_n8():
-	data_file = h5py.File('./data/raw/TNG300/mock_TNG300.hdf5', 'a')
-	COM = data_file["COM"][:]
-	Semimajor_Axis_Direction = data_file["Semimajor_Axis_Direction"][:]
-	q = data_file["q"][:]
-	data_file.close()
-	data_dir = {
-		"Position": COM,
-		"Position_shape_sample": COM,
-		"Axis_Direction": Semimajor_Axis_Direction,
-		"LOS": 2,
-		"q": q
-	}
-	return MeasureIABox(data_dir, f"./data/processed/TNG300/test_IA_mock_TNG300_jk.hdf5",
-						"TNG300", 99, [0.1, 20], 10,
-						8, None, num_nodes=8)
